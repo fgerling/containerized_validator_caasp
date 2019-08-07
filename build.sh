e4359f5d104c898a51bdb7a07c181a05566d4631
fgerling <felix@fgerling.de>
2019-08-07 15:56:40 +0200

    Make sonobuoy's version, os and arch configurable at image build time

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 #! /bin/sh
-## get sonobuoy v0.15.0
-if test ! -f ./sonobuoy_0.15.0_darwin_amd64.tar.gz
+## get sonobuoy v0.15.1
+SONOBUOY_VERSION="0.15.1"
+SONOBUOY_OS="linux"
+SONOBUOY_ARCH="amd64"
+sonobuoy_tar="sonobuoy_${SONOBUOY_VERSION}_${SONOBUOY_OS}_${SONOBUOY_ARCH}.tar.gz"
+if test ! -f ./${sonobuoy_tar}
 then
-	curl -O -L https://github.com/heptio/sonobuoy/releases/download/v0.15.0/sonobuoy_0.15.0_darwin_amd64.tar.gz
-	tar xvf sonobuoy_0.15.0_darwin_amd64.tar.gz;
+	curl -O -L "https://github.com/heptio/sonobuoy/releases/download/v${SONOBUOY_VERSION}/${sonobuoy_tar}"
+	tar xvf $sonobuoy_tar
 fi
 
 podman build -t validator_caasp:latest .
